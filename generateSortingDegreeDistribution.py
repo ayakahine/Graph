1fde87ef32f2a065919383555cdae52919587e8e
ayakahine <ayakahine@users.noreply.github.com>
2017-12-01 14:54:05 +0100

    Update generateSortingDegreeDistribution.py

--- generateSortingDegreeDistribution.py
+++ generateSortingDegreeDistribution.py
@@ -1,1 +1,17 @@
+import random
 
+
+def degree_distribution(length, upper_bound):
+    if length == 0:
+        return []
+    else:
+        return [random.randint(1, upper_bound)] + degree_distribution(length - 1, upper_bound)
+
+
+def generate_degree_distribution(length, upper_bound):
+    result = degree_distribution(length, upper_bound)
+    result.sort(reverse=True)
+    return result
+
+
+print generate_degree_distribution(5, 4)
